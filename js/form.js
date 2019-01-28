ad21359e299fe5a9ce1c6b7c2653b312e7efce14
tomhet <escape.outerspace@gmail.com>
2019-01-28 11:02:51 +0500

    added apply effect on click function

--- js/form.js
+++ js/form.js
@@ -1,11 +1,36 @@
 'use strict';
 
 (function () {
-  // Constants
   var Scale = {
     MIN: 25,
     MAX: 100,
     STEP: 25
+  };
+
+  var Effect = {
+    NONE: {
+      id: 'effect-none',
+    },
+    CHROME: {
+      id: 'effect-chrome',
+      class: 'effects__preview--chrome'
+    },
+    SEPIA: {
+      id: 'effect-sepia',
+      class: 'effects__preview--sepia'
+    },
+    MARVIN: {
+      id: 'effect-marvin',
+      class: 'effects__preview--marvin'
+    },
+    PHOBOS: {
+      id: 'effect-phobos',
+      class: 'effects__preview--phobos'
+    },
+    HEAT: {
+      id: 'effect-heat',
+      class: 'effects__preview--heat'
+    }
   };
 
   var uploadButton = document.querySelector('#upload-file');
@@ -14,10 +39,16 @@
   var scaleControlValue = uploadOverlay.querySelector('.scale__control--value');
   var scaleControlSmaller = uploadOverlay.querySelector('.scale__control--smaller');
   var scaleControlBigger = uploadOverlay.querySelector('.scale__control--bigger');
+  var effectsList = uploadOverlay.querySelector('.effects__list');
+  var effectLevel = uploadOverlay.querySelector('.effect-level');
+  var effectLevelPin = effectLevel.querySelector('.effect-level__pin');
+  var effectLevelDepth = effectLevel.querySelector('.effect-level__depth');
+  var effectLevelValue = effectLevel.querySelector('.effect-level__value');
   var hashtagsInput = uploadOverlay.querySelector('.text__hashtags');
   var descriptionInput = uploadOverlay.querySelector('.text__description');
   var imageUploadPreview = document.querySelector('.img-upload__preview');
   var imagePreview = imageUploadPreview.querySelector('img');
+  var activeEffect;
 
   // Hide on ESC keydown
   var onUploadEscPress = function (evt) {
@@ -36,24 +67,38 @@
     document.removeEventListener('keydown', onUploadEscPress);
     scaleControlSmaller.removeEventListener('click', onScaleControlSmallerClick);
     scaleControlBigger.removeEventListener('click', onScaleControlBiggerClick);
+    effectsList.removeEventListener('click', setEffect);
   };
 
   // Default form settings
-  var resetEffects = function () {
+  var resetScale = function () {
     imagePreview.style.transform = null;
     scaleControlValue.value = '100%';
+  };
+
+  var resetEffect = function () {
+    if (activeEffect) {
+      imagePreview.classList.remove(activeEffect.class);
+    }
+
+    effectLevelPin.style.left = '100%';
+    effectLevelDepth.style.width = '100%';
+    effectLevelValue.value = '100';
+    effectLevel.classList.add('hidden');
   };
 
   // Open form
   var onUploadButtonClick = function () {
     uploadOverlay.classList.remove('hidden');
     window.photo.readFile();
-    resetEffects();
+    resetScale();
+    resetEffect();
 
     uploadCancelButton.addEventListener('click', hideUploadOverlay);
     document.addEventListener('keydown', onUploadEscPress);
     scaleControlSmaller.addEventListener('click', onScaleControlSmallerClick);
     scaleControlBigger.addEventListener('click', onScaleControlBiggerClick);
+    effectsList.addEventListener('click', setEffect);
   };
 
   // Change image scale
@@ -77,6 +122,37 @@
     if (value < Scale.MAX) {
       value += Scale.STEP;
       setScale(value);
+    }
+  };
+
+  // Set effect to image
+  var getEffect = function (evt) {
+    activeEffect = evt.target;
+
+    switch (activeEffect.id) {
+      case Effect.CHROME.id:
+        return Effect.CHROME;
+      case Effect.SEPIA.id:
+        return Effect.SEPIA;
+      case Effect.MARVIN.id:
+        return Effect.MARVIN;
+      case Effect.PHOBOS.id:
+        return Effect.PHOBOS;
+      case Effect.HEAT.id:
+        return Effect.HEAT;
+      default:
+        return '';
+    }
+  };
+
+  var setEffect = function (evt) {
+    resetEffect();
+
+    activeEffect = getEffect(evt);
+
+    if (activeEffect !== '') {
+      effectLevel.classList.remove('hidden');
+      imagePreview.classList.add(activeEffect.class);
     }
   };
 
