8baf99cb6e3fd275e8f9998a978fc9725cf8f633
tomhet <escape.outerspace@gmail.com>
2019-02-05 21:51:11 +0500

    added filters in pictures.js

--- js/pictures.js
+++ js/pictures.js
@@ -1,11 +1,24 @@
 'use strict';
 
 (function () {
+
+  var Filter = {
+    POPULAR: 'filter-popular',
+    NEW: 'filter-new',
+    DISCUSSED: 'filter-discussed'
+  };
+
+  var NEW_PICTURES_COUNT = 10;
+
   var template = document.querySelector('#picture');
   var pictureTemplate = template.content.querySelector('.picture');
   var picturesDOM = document.querySelector('.pictures');
+  var imgFilters = document.querySelector('.img-filters');
+  var imgFiltersForm = imgFilters.querySelector('.img-filters__form');
   var pictures = [];
 
+
+  // Get picture DOM
   var getPicture = function (picture) {
     var pictureElement = pictureTemplate.cloneNode(true);
 
@@ -16,6 +29,26 @@
     return pictureElement;
   };
 
+
+  // Sort pictures by comments number
+  var sortByComments = function (pics) {
+    pics = pics.concat([]);
+
+    pics.sort(function (a, b) {
+      if (a.comments.length > b.comments.length) {
+        return -1;
+      } else if (a.comments.length < b.comments.length) {
+        return 1;
+      }
+
+      return 0;
+    });
+
+    return pics;
+  };
+
+
+  // Render pictures on page
   var renderPictures = function (photos) {
     var fragment = document.createDocumentFragment();
 
@@ -34,10 +67,19 @@
     picturesDOM.appendChild(fragment);
   };
 
+  // Remove pictures
+  var removePictures = function () {
+    var photos = picturesDOM.querySelectorAll('.picture');
+
+    photos.forEach(function (elem) {
+      picturesDOM.removeChild(elem);
+    });
+  };
+
   var onSuccessDownload = function (arr) {
     pictures = arr.slice();
-
     renderPictures(arr);
+    imgFilters.classList.remove('img-filters--inactive');
   };
 
   var onErrorDownload = function (errorMessage) {
@@ -66,5 +108,61 @@
   };
 
   window.backend.downloadData(onSuccessDownload, onErrorDownload);
+
+
+  // Filters
+  var showPopularPictures = function (pics) {
+    removePictures();
+    renderPictures(pics);
+  };
+
+  var showNewPictures = function (pics) {
+    pics = pics.concat([]);
+    pics.sort(function () {
+      return Math.random() - 0.5;
+    });
+
+    removePictures();
+    renderPictures(pics.slice(0, NEW_PICTURES_COUNT));
+  };
+
+  var showDiscussedPictures = function (pics) {
+    var sortedPics = sortByComments(pics);
+
+    removePictures();
+    renderPictures(sortedPics);
+  };
+
+  var addActiveClass = function (evt) {
+    var activeButton = document.querySelector('.img-filters__button--active');
+    activeButton.classList.toggle('img-filters__button--active');
+
+    evt.target.classList.add('img-filters__button--active');
+  };
+
+  var switchFilter = window.debounce(function (effect) {
+    switch (effect.id) {
+      case Filter.POPULAR:
+        showPopularPictures(pictures);
+        break;
+      case Filter.NEW:
+        showNewPictures(pictures);
+        break;
+      case Filter.DISCUSSED:
+        showDiscussedPictures(pictures);
+        break;
+      default:
+        break;
+    }
+  });
+
+  var onFilterClick = function (evt) {
+    if (evt.target.type === 'button') {
+      addActiveClass(evt);
+      switchFilter(evt.target);
+    }
+  };
+
+  imgFiltersForm.addEventListener('click', onFilterClick);
 
 })();
